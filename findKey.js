0e844a0bca7f6a6d07a5d2df166df2b33245641f
Lazar Vujanovic <l.vujanovic.97@gmail.com>
2021-09-16 21:56:41 +0000

    function to find a particular key of an object and return it

---
+++ findKey.js
@@ -0,0 +1,34 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function (object, callback) {
+  let keysList = Object.keys(object);
+  for (let key of keysList) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+
+const results1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+assertEqual(results1, "noma");
+assertEqual(results1, "Ora");
+
+
+
+
